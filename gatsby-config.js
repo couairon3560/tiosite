8a63fa085c3d4d3097074cb455f9f064f92898b1
couairon3560 <81307275+couairon3560@users.noreply.github.com>
2021-04-02 23:58:02 +0200

    Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,37 @@
 module.exports = {
   siteMetadata: {
     title: "tiosite",
+    description: `Test pixel`,
+    author: `@gatsbyjs`,
   },
   plugins: [
+     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
     {
-      resolve: "gatsby-source-sanity",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        projectId: "y3n5tmwy",
-        dataset: "staging",
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
-    "gatsby-plugin-styled-components",
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   {
     resolve: `gatsby-plugin-facebook-pixel`,
     options: {
